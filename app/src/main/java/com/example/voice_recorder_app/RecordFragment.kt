7ef465cbabe09b5659a3fd0a1ddebd30ff997690
upon0426 <karasawa.yui@gmail.com>
2020-10-29 12:44:14 +0900

    WIP record function

--- app/src/main/java/com/example/voice_recorder_app/RecordFragment.kt
+++ app/src/main/java/com/example/voice_recorder_app/RecordFragment.kt
@@ -3,7 +3,9 @@ package com.example.voice_recorder_app
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.media.Image
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,11 +18,15 @@ import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import java.io.File
 import java.util.jar.Manifest
 
 
-class RecordFragment(private var isRecording: Boolean = false) : Fragment(), View.OnClickListener {
+class RecordFragment() : Fragment(), View.OnClickListener {
 
+    private var isRecording: Boolean = false
+    private var mediaRecorder: MediaRecorder? = null
+    private var recordFile: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,10 +47,12 @@ class RecordFragment(private var isRecording: Boolean = false) : Fragment(), Vie
             R.id.record_list_btn -> findNavController().navigate(R.id.action_recordFragment_to_audioListFragment)
             R.id.record_btn -> {
                 if (isRecording) {
+                    startRecording()
                     view.findViewById<ImageButton>(R.id.record_btn).setImageResource(R.drawable.stop_recording)
                     isRecording = false
                 } else {
                     if (checkPermissions()) {
+                        stopRecording()
                         view.findViewById<ImageButton>(R.id.record_btn)
                             .setImageResource(R.drawable.record)
                         isRecording = true
@@ -52,6 +60,27 @@ class RecordFragment(private var isRecording: Boolean = false) : Fragment(), Vie
                 }
             }
         }
+    }
+
+    private fun stopRecording() {
+        mediaRecorder?.stop()
+        mediaRecorder?.release()
+        mediaRecorder = null
+    }
+
+    private fun startRecording() {
+        var recordPath: File? = File(context?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "voice_recorder")
+        recordFile = "Filename.3bg"
+
+        mediaRecorder = MediaRecorder()
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecorder?.setOutputFile(recordPath.toString() + "/" + recordFile)
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+        mediaRecorder?.prepare()
+        mediaRecorder?.start()
+
     }
 
     private fun checkPermissions(): Boolean {
