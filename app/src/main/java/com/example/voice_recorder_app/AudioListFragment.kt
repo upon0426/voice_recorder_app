9717563cca39b21526e60ee52640dfb907eef220
upon0426 <karasawa.yui@gmail.com>
2020-10-30 23:46:55 +0900

    made playsheet not to hide

--- app/src/main/java/com/example/voice_recorder_app/AudioListFragment.kt
+++ app/src/main/java/com/example/voice_recorder_app/AudioListFragment.kt
@@ -5,18 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AudioListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AudioListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +18,24 @@ class AudioListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_audio_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val playerSheet: ConstraintLayout = view.findViewById<ConstraintLayout>(R.id.player_sheet)
+        val bottomSheetBehavior = BottomSheetBehavior.from(playerSheet)
+
+        bottomSheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // do nothing
+            }
+        })
     }
 
 }
