90c96e98c836bd44cd59ac893301a4f47c2b82d7
upon0426 <karasawa.yui@gmail.com>
2020-10-31 01:37:46 +0900

    made the file visible

---
+++ app/src/main/java/com/example/voice_recorder_app/AudioListAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.voice_recorder_app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.io.File
+
+class AudioListAdapter(
+    var allFiles: Array<File>?
+): RecyclerView.Adapter<AudioListAdapter.AudioViewHolder>() {
+    class AudioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val list_image = itemView.findViewById<ImageView>(R.id.list_image_view)
+        val list_title = itemView.findViewById<TextView>(R.id.list_title)
+        val list_date = itemView.findViewById<TextView>(R.id.list_date)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AudioViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_list_item, parent, false)
+        return AudioViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AudioViewHolder, position: Int) {
+        holder.list_title.text = allFiles?.get(position)?.name
+    }
+
+    override fun getItemCount(): Int {
+        return if (allFiles.isNullOrEmpty()) {
+            0
+        } else {
+            allFiles!!.size
+        }
+    }
+}
--- app/src/main/java/com/example/voice_recorder_app/AudioListFragment.kt
+++ app/src/main/java/com/example/voice_recorder_app/AudioListFragment.kt
@@ -1,13 +1,18 @@
 package com.example.voice_recorder_app
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import java.io.File
 
 class AudioListFragment : Fragment() {
 
@@ -22,10 +27,23 @@ class AudioListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val playerSheet: ConstraintLayout = view.findViewById<ConstraintLayout>(R.id.player_sheet)
+        val playerSheet: ConstraintLayout = view.findViewById(R.id.player_sheet)
         val bottomSheetBehavior = BottomSheetBehavior.from(playerSheet)
+        val audioList: RecyclerView = view.findViewById(R.id.audio_list_view)
 
-        bottomSheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
+        val path: String? = activity?.getExternalFilesDir("/")?.absolutePath
+        val directory = File(path).parentFile
+        val allFiles = directory.listFiles()
+        //Log.e(TAG, directory.listFiles()[3].name)
+
+        val audioListAdapter = AudioListAdapter(allFiles)
+
+        audioList.setHasFixedSize(true)
+        audioList.layoutManager = LinearLayoutManager(context)
+        audioList.adapter = audioListAdapter
+
+
+            bottomSheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                     bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
